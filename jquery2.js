5fe908155c41daf5acc96c5b5cc78ac0b33ce51b
ttabithar <ttabithar@gmail.com>
2024-02-17 17:02:31 -0600

    prep

---
+++ jquery2.js
@@ -0,0 +1,13 @@
+// add the tabs widget 
+
+// creater play game page with images array of object with 1. front 2. back 3. blank
+
+// based on waht user wants, randomly select (from grab half of that from array and duplicate it forr ther (matching)
+
+// div id cards
+    // div row 1
+        // each card <a> elemnt with id set to eh src attrivte for card, and its href attribute set to #. the <a> elemtn contain img elemnt with its srt attrive set to eh source image for the back of t he card and its lat attributer set to ""
+    // div row 2
+    // div row 3
+
+    // animate for the cards
